36b17d334ec44edf4e10c014e416822fad5e4d9a
Arthur Touati <134126573+NFXSTUDIO@users.noreply.github.com>
2025-04-03 10:12:49 +0200

    add ex1

--- exercice_1.py
+++ exercice_1.py
@@ -1,6 +1,9 @@
 from sympy import symbols as sb
 
-x,y,z,t,a,b,c = sb('x,y,z,t,a,b,c')
+x,y,z,t,c = sb('x,y,z,t,c')
+a,b = sb('a,b')
+
+# Simplification :
 expr = 4*x+3*y-5*z+4*t-3*(x+y+z+t)
 print(expr.simplify())
 
@@ -8,6 +11,7 @@ expr2 = x**2+2*x*y
 expr3 = x**4-5*x**3
 expr4 = y+y**2*t
 
+# Factorisation :
 print("\nfactorise : \n")
 print(expr2.factor()) #factorise expression
 print(expr3.factor())
@@ -17,7 +21,24 @@ expr5 = a*(3*a+b+c)
 expr6 = x**2*(x**2+x*y)
 expr7 = x*y*z*(x+y+z)
 
+# Development :
 print("\nexpand : \n")
 print(expr5.expand()) #develop expression
 print(expr6.expand())
 print(expr7.expand())
+
+# Substitution :
+expr8 = x+1
+print("Substitute : ")
+print(expr8.subs(x,2))
+
+#fonction :
+f = 5*x**2+4*x+3
+g = 4*x/(3*x+2)
+h = 7*x*(3*x+1/x)
+
+print("Fonction :")
+print(f.subs(x,3))
+print(g.subs(x,-1/3))
+print(g.subs(x,-2/3))
+print(h.subs(x,4))
